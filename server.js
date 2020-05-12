4150521fcc57974c48a326a352d7e9d48d27c451
enes.sefa.k <enes.sefa.k@gmail.com>
2020-05-13 01:17:59 +0300

    dneme

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'pug')
 
