2bc719e17c7f1c927d8f2e0889b4af433f39e9d8
enes.sefa.k <enes.sefa.k@gmail.com>
2020-05-13 01:56:09 +0300

    change

--- assets/index.js
+++ assets/index.js
@@ -4,6 +4,6 @@ const {appId, redirectUri, scope } = {
     scope: 'user_profile,user_media'
 }
 
-fetch(`https://api.instagram.com/oauth/authorize?client_id=${appId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=code`)
+fetch(`https://api.instagram.com/oauth/authorize?client_id=2941726619215643&redirect_uri=https://senintablon.herokuapp.com/auth&scope=user_profile,user_media&response_type=code`)
 .then(res => res.json())
 .then(data => console.log(data))
